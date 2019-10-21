bf4b98a9edde4d272e70ed20b791721c8685aa2e
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-21 19:23:16 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,12 +2,20 @@ module Findable
   
   module ClassMethods
     
-    def find_by_name(name)
-      self.all.detect{|a| a.name == name}
+    def find_by_name(given_name)
+      self.all.find {|a| a.name == given_name}
     end
     
     def all
       class_variable_get("@@all")
+    end
+    
+    def find_or_create_by_name(name)
+      if !find_by_name(name)
+        self.create(name)
+      else
+        find_by_name(name)
+      end
     end
     
   end
