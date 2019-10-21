21b20f9840592635083b38aef116f1a94c959f00
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-21 19:11:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
   
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist
-    self.genre = genre
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
   
   def artist=(artist)
@@ -21,7 +21,7 @@ class Song
   end
   
   def genre=(genre)
-    self.genre = genre if genre
+    @genre = genre if genre
     genre.songs << self unless genre.songs.include?(self)
   end
   
