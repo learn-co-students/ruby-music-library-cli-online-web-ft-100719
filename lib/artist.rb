862347668d4a324da76154daa73f06e19824ed23
Geoff Elliott <tinywv@gmail.com>
2019-10-25 11:24:57 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    unless song.artist == self
+      song.artist = self
+    end
+    unless @songs.include?(song)
+      @songs << song
+    end
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    self.artist = artist if artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def artist
+    @artist
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
+end
