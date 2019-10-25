8c1963d242d6f32e61bd0b0d1ba3f2e2b69200c3
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-25 02:51:18 +0000

    Done.

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -72,7 +72,9 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     user_input = gets.strip
     if chosen_artist = Artist.find_by_name(user_input)
-      chosen_artist.songs.sort_by{ |song| song.name}.each do
+      sorted_songlist = chosen_artist.songs.sort_by{ |song| song.name}
+      #binding.pry
+      sorted_songlist.each_with_index do |this_song, index|
         puts "#{index+1}. #{this_song.name} - #{this_song.genre.name}"
       end #do
     end#if
